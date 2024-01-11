91195c9db95e07787d5a63852ad8986379199973
loladenney <156242030+loladenney@users.noreply.github.com>
2024-01-11 15:57:58 -0500

    Update README.ml

--- README.ml
+++ README.ml
@@ -1,4 +1,5 @@
 Lola Denney's Personal Portfolio
 
 About Me:
-I am a 3rd year student working on my Bachelor of Computer Science at McGill University in Montreal
+I am a 3rd year student working on my Bachelor of Computer Science at McGill University in Montreal. 
+
